3e121466648eb7edffa8ec79ee8e89c30e0c8677
lodaogos <leecharles565@gmail.com>
2023-12-19 10:06:01 +0700

    Ubah services agar sesuai kos

--- database/migrations/2023_11_22_134955_create_services_table.php
+++ database/migrations/2023_11_22_134955_create_services_table.php
@@ -11,43 +11,43 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('services', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-            $table->string('title');
-            $table->string('description');
-            $table->string('basic_plan_title');
-            $table->integer('basic_plan_price');
-            $table->string('basic_plan_description');
-            $table->string('basic_plan_days');
-            $table->string('standard_plan_title');
-            $table->integer('standard_plan_price');
-            $table->string('standard_plan_description');
-            $table->string('standard_plan_days');
-            $table->string('premium_plan_title');
-            $table->integer('premium_plan_price');
-            $table->string('premium_plan_description');
-            $table->string('premium_plan_days');
-            $table->string('image')->nullable();
-            $table->decimal('average_star', 2, 1);
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('category_id')->constrained('subcategory');
-            $table->foreignId('subcategory_id')->constrained('subcategory');
-        });
-
         // Schema::create('services', function (Blueprint $table) {
         //     $table->id();
         //     $table->timestamps();
         //     $table->string('title');
         //     $table->string('description');
-
+        //     $table->string('basic_plan_title');
+        //     $table->integer('basic_plan_price');
+        //     $table->string('basic_plan_description');
+        //     $table->string('basic_plan_days');
+        //     $table->string('standard_plan_title');
+        //     $table->integer('standard_plan_price');
+        //     $table->string('standard_plan_description');
+        //     $table->string('standard_plan_days');
+        //     $table->string('premium_plan_title');
+        //     $table->integer('premium_plan_price');
+        //     $table->string('premium_plan_description');
+        //     $table->string('premium_plan_days');
         //     $table->string('image')->nullable();
         //     $table->decimal('average_star', 2, 1);
         //     $table->foreignId('user_id')->constrained();
-
         //     $table->foreignId('category_id')->constrained('subcategory');
         //     $table->foreignId('subcategory_id')->constrained('subcategory');
         // });
+
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->string('image')->nullable();
+            $table->decimal('average_star', 2, 1);
+            $table->integer('kamar_tersedia');
+            $table->integer('harga_per_bulan');
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('category_id')->constrained('subcategory');
+            $table->foreignId('subcategory_id')->constrained('subcategory');
+        });
     }
 
     /**
