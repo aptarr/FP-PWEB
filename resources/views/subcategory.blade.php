69a3bb78a310baece7b904c3df7f549c9aac691c
aptarr <apta.rasendriya@gmail.com>
2023-12-21 03:20:27 +0700

    fixing: layout list Kost

--- resources/views/subcategory.blade.php
+++ resources/views/subcategory.blade.php
@@ -11,189 +11,103 @@
         }
     </style>
 
-    <div class="flex items-center justify-center bg-white">
-        <div class="w-4/5 min-h-800">
-            <div class="mt-10 text-3xl font-bold">
+    <div class="w-full min-h-screen bg-white p-6 grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div>
+            <h1 class="font-bold text-4xl text-center md:text-3xl md:mt-12 mb-2">{{$subcategoryModel->subcategory_name}}
+            </h1>
+            <h1 class="text-xl text-center md:text-3xl mb-4">Semua kos yang ada di
                 {{$subcategoryModel->subcategory_name}}
-            </div>
-            <div class="mt-5 text-xl">
-                Semua kos yang ada di {{$subcategoryModel->subcategory_name}}
-            </div>
+            </h1>
 
-            <div class="mt-10 border border-t-gray-300 w-full">
-            </div>
-
-            <div class="">
-                <div class="mt-10">
-                    <div class="flex">
-                        <div class="font-bold border border-gray-300 rounded-lg w-32 py-3 text-center tab inactive flex justify-center"
-                            data-tab="budget">
-                            Harga
-                            <svg class="ml-2 h-6 w-6 text-black" width="24" height="24" viewBox="0 0 24 24"
-                                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path
-                                    d="M5.5 5h13a1 1 0 0 1 0.5 1.5L14 12L14 19L10 16L10 12L5 6.5a1 1 0 0 1 0.5 -1.5" />
-                            </svg>
-                        </div>
-                        <div class="hidden font-bold border border-gray-300 rounded-lg w-40 py-3 ml-4 text-center tab inactive justify-center"
-                            data-tab="delivery">
-                            Delivery time
-                            <svg class="ml-2 h-6 w-6 text-black" width="24" height="24" viewBox="0 0 24 24"
-                                stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                stroke-linejoin="round">
-                                <path stroke="none" d="M0 0h24v24H0z" />
-                                <path
-                                    d="M5.5 5h13a1 1 0 0 1 0.5 1.5L14 12L14 19L10 16L10 12L5 6.5a1 1 0 0 1 0.5 -1.5" />
-                            </svg>
-                        </div>
-                    </div>
-
-                    <div class="tab-content hidden mt-2 rounded-lg border border-gray-300 p-5 w-64" id="budgetContent"
-                        data-subcategory="{{ $subcategoryModel->subcategory_name }}">
-                        <input id="budgetValue" name="budgetHidden" class="hidden"
-                            value="{{$budgetLower}}-{{$budgetUpper}}">
-                        <!-- <div id="budgetValue" class="hidden" data-subcategory="{{$budgetLower}}-{{$budgetUpper}}"></div> -->
-
-                        <div class="flex items-center">
-                            <input id="default-radio-1" type="radio" value="0-300000" name="budget-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2">
-                            <label for="default-radio-1" class="ms-2 text-base font-bold">Value</label>
-                            <div class="ml-5">Dibawah Rp300.000</div>
-                        </div>
-                        <div class="flex items-center mt-2">
-                            <input checked id="default-radio-2" type="radio" value="300000-700000" name="budget-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
-                            <label for="default-radio-2" class="ms-2 text-base font-bold ">Mid-range</label>
-                            <div class="ml-5">Rp300.000 - Rp700.000</div>
-                        </div>
-                        <div class="flex items-center mt-2">
-                            <input checked id="default-radio-3" type="radio" value="700000-1000000" name="budget-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
-                            <label for="default-radio-3" class="ms-2 text-base font-bold ">High-end</label>
-                            <div class="ml-5">Diatas Rp700.000</div>
-                        </div>
-
-                        <div class="mt-3 flex justify-center">
-                            <a href="#" onclick="applyFilters()"
-                                class="block px-2 py-1 hover:bg-gray-200 font-extrabold">
-                                <button type="button"
-                                    class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-md text-sm px-20 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30">
-                                    Apply
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="ml-36 tab-content hidden mt-2 rounded-lg border border-gray-300 p-5 w-64"
-                        id="deliveryContent">
-                        <input id="timeHidden" name="timeHidden" class="hidden" value="{{$time}}">
-                        <div class="flex items-center">
-                            <input id="default-radio-5" type="radio" value="1" name="time-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2">
-                            <label for="default-radio-5" class="ms-2 text-base font-bold">Express 24H</label>
-                        </div>
-                        <div class="flex items-center mt-2">
-                            <input checked id="default-radio-6" type="radio" value="3" name="time-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
-                            <label for="default-radio-6" class="ms-2 text-base font-bold ">Up to 3 days</label>
-                        </div>
-                        <div class="flex items-center mt-2">
-                            <input checked id="default-radio-7" type="radio" value="7" name="time-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
-                            <label for="default-radio-7" class="ms-2 text-base font-bold ">Up to 7 days</label>
-                        </div>
-                        <div class="flex items-center mt-2">
-                            <input checked id="default-radio-8" type="radio" value="999" name="time-radio"
-                                class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
-                            <label for="default-radio-8" class="ms-2 text-base font-bold ">Anytime</label>
-                        </div>
-                        <div class="mt-3 flex justify-center">
-                            <a href="#" onclick="applyFiltersTime()"
-                                class="block px-2 py-1 hover:bg-gray-200 font-extrabold">
-                                <button type="button"
-                                    class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-md text-sm px-20 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30">
-                                    Apply
-                                </button>
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="w-full mt-4 p-4">
-                        @php
-                        $filteredServices = $services->filter(function ($service) use ($budgetLower, $budgetUpper,
-                        $time) {
-                        return $service->harga_per_bulan >= $budgetLower
-                        && $service->harga_per_bulan <= $budgetUpper; }); $servicesChunks=$filteredServices->chunk(5);
-                            @endphp
-                            @foreach($servicesChunks as $serviceChunk)
-                            <div class="flex w-full mt-5 gap-5">
-                                @foreach($serviceChunk as $service)
-                                <div class="w-56">
-                                    <div id="" class="relative w-56 h-52">
-                                        <!-- Carousel wrapper -->
-                                        <div class="relative overflow-hidden rounded-lg md:h-52">
-                                            <!-- Item 1 -->
-                                            <div>
-                                                <a href="{{ route('service.show', ['id' => $service->id, 'user_id' => $service->user_id]) }}"
-                                                    class="text-decoration-none">
-                                                    @if(isset($service->image))
-                                                    <img src="{{ Storage::url($service->image) }}"
-                                                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                                                        alt="image-service">
-                                                    @else
-                                                    <img src="{{ asset('images/Thinker-Auguste-Rodin-Museum-Paris-1904.jpg') }}"
-                                                        class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-                                                        alt="...">
-                                                    @endif
-                                                </a>
-                                            </div>
-                                        </div>
-                                    </div>
-
-                                    <div class="mt-1 text-lg">
-                                        <a href="{{ route('service.show', ['id' => $service->id, 'user_id' => $service->user_id]) }}"
-                                            class="text-decoration-none hover:underline">
-                                            {{ $service->title}}
-                                        </a>
-                                    </div>
-
-                                    <div class="mt-4 flex items-center">
-                                        <svg class="h-4 w-4 black width=" 24" height="24" viewBox="0 0 24 24"
-                                            stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" />
-                                            <path
-                                                d="M12 17.75l-6.172 3.245 1.179-6.873-4.993-4.867 6.9-1.002L12 2l3.086 6.253 6.9 1.002-4.993 4.867 1.179 6.873"
-                                                fill="currentColor" />
-                                        </svg>
-                                        <div class="ml-1">
-                                            {{ $service->average_star}}
-                                        </div>
-                                        <div class="ml-1">
-                                            (
-                                        </div>
-                                        <div class="">
-                                            {{ $service->total_reviews}}
-                                        </div>
-                                        <div>
-                                            )
-                                        </div>
-                                    </div>
-                                    <div class="font-bold">
-                                        <a href="{{ route('service.show', ['id' => $service->id, 'user_id' => $service->user_id]) }}"
-                                            class="text-decoration-none hover:underline">
-                                            Harga Rp{{ number_format($service->harga_per_bulan, 0, ',', '.') }}
-                                        </a>
-                                    </div>
-                                </div>
-                                @endforeach
-                            </div>
-                            @endforeach
-                    </div>
+            <div class="flex justify-center sm:w-8/12 mx-auto">
+                <div class="font-bold border border-gray-300 rounded-lg w-32 py-3 text-center tab inactive flex justify-center"
+                    data-tab="budget">
+                    Harga
+                    <svg class="ml-2 h-6 w-6 text-black" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <path d="M5.5 5h13a1 1 0 0 1 0.5 1.5L14 12L14 19L10 16L10 12L5 6.5a1 1 0 0 1 0.5 -1.5" />
+                    </svg>
+                </div>
+                <div class="hidden font-bold border border-gray-300 rounded-lg w-40 py-3 ml-4 text-center tab inactive justify-center"
+                    data-tab="delivery">
+                    Delivery time
+                    <svg class="ml-2 h-6 w-6 text-black" width="24" height="24" viewBox="0 0 24 24" stroke-width="2"
+                        stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round">
+                        <path stroke="none" d="M0 0h24v24H0z" />
+                        <path d="M5.5 5h13a1 1 0 0 1 0.5 1.5L14 12L14 19L10 16L10 12L5 6.5a1 1 0 0 1 0.5 -1.5" />
+                    </svg>
                 </div>
             </div>
+
+            <div class="tab-content hidden mt-2 rounded-lg border border-gray-300 p-5 w-64 mx-auto" id="budgetContent"
+                data-subcategory="{{ $subcategoryModel->subcategory_name }}">
+                <input id="budgetValue" name="budgetHidden" class="hidden" value="{{$budgetLower}}-{{$budgetUpper}}">
+                <!-- <div id="budgetValue" class="hidden" data-subcategory="{{$budgetLower}}-{{$budgetUpper}}"></div> -->
+
+                <div class="flex items-center">
+                    <input id="default-radio-1" type="radio" value="0-300000" name="budget-radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2">
+                    <label for="default-radio-1" class="ms-2 text-base font-bold">Value</label>
+                    <div class="ml-5">Dibawah Rp300.000</div>
+                </div>
+                <div class="flex items-center mt-2">
+                    <input checked id="default-radio-2" type="radio" value="300000-700000" name="budget-radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
+                    <label for="default-radio-2" class="ms-2 text-base font-bold ">Mid-range</label>
+                    <div class="ml-5">Rp300.000 - Rp700.000</div>
+                </div>
+                <div class="flex items-center mt-2">
+                    <input checked id="default-radio-3" type="radio" value="700000-1000000" name="budget-radio"
+                        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 ">
+                    <label for="default-radio-3" class="ms-2 text-base font-bold ">High-end</label>
+                    <div class="ml-5">Diatas Rp700.000</div>
+                </div>
+
+                <div class="mt-3 flex justify-center">
+                    <a href="#" onclick="applyFilters()" class="block px-2 py-1 hover:bg-gray-200 font-extrabold">
+                        <button type="button"
+                            class="text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-md text-sm px-20 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#050708]/30">
+                            Apply
+                        </button>
+                    </a>
+                </div>
+            </div>
+            @php
+            $filteredServices = $services->filter(function ($service) use ($budgetLower, $budgetUpper,
+            $time) {
+            return $service->harga_per_bulan >= $budgetLower
+            && $service->harga_per_bulan <= $budgetUpper; }); $servicesChunks=$filteredServices->chunk(2);
+                @endphp
+                @foreach($servicesChunks as $serviceChunk)
+                <div class="flex flex-wrap justify-center">
+                    @foreach($serviceChunk as $service)
+                    <div class="flex flex-col bg-white rounded-lg shadow-md w-full overflow-hidden sm:w-96 p-5 m-3">
+                        @if(isset($service->image))
+                        <img src="{{ Storage::url($service->image) }}" class="h-50 w-full object-cover rounded-md"
+                            alt="image-service">
+                        @else
+                        <img src="{{ asset('images/gambar_1.jpg') }}" class="h-50 w-full object-cover rounded-md"
+                            alt="...">
+                        @endif
+                        <div class="mt-1 text-lg">
+                            <a href="{{ route('service.show', ['id' => $service->id, 'user_id' => $service->user_id]) }}"
+                                class="text-decoration-none hover:underline mt-2 text-2xl font-bold text-stone-900">
+                                {{ $service->title}}
+                            </a>
+                        </div>
+                        <p class="mt-2 text-stone-900">
+                            testing
+                        </p>
+                    </div>
+                    @endforeach
+                </div>
+                @endforeach
+        </div>
+        <div class="flex flex-col h-full md:h-full">
+            <iframe
+                src="https://www.google.com/maps/d/u/0/embed?mid=1osXGNZ1kNz_Qvg8CEHqoRVmnLLDgtuc&ehbc=2E312F&noprof=1"
+                class="w-full h-96 md:h-full" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
         </div>
     </div>
 
